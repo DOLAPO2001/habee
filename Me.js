b9b6f2546a1dade3ade5d467700645bc09c9beee
DOLAPO2001 <106277169+DOLAPO2001@users.noreply.github.com>
2022-07-23 14:09:40 +0100

    Add files via upload

---
+++ Me.js
@@ -0,0 +1,23 @@
+function sayHello(name){
+  alert('Hi ' + name);
+}
+sayHello('Visitors');
+ function myman(name){
+   alert('This is robot Created By ' + name)
+ }
+ myman("Habeeb");
+
+var firstName = prompt("What is your Name");
+var secondName = prompt("What is Their Name")
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+if (loveScore === 100) {
+  alert( firstName + ' and '+ secondName+ " Your love score is " + loveScore + " %" + "Actually You guys are the best Couple❤️❤️");
+}
+else if (loveScore > 50) {
+  alert( firstName + ' and '+ secondName + " Your love score is " + loveScore + " %" + " Actually You guys are the worst Couple😋😋");
+}
+else
+alert(firstName + " and "+ secondName + " Your love score is " + loveScore + " %" + "🤣🤣🤣😂 e bad oo i pity you");
+
+
